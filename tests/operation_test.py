7fd3bca0be55975dd9bcd8ecfcc2a77cf07fa38d
ankur33010 <114218720+ankur33010@users.noreply.github.com>
2024-12-25 20:43:26 +0530

    Unit Case Updated

---
+++ tests/operation_test.py
@@ -0,0 +1,10 @@
+from src.Maths_Operations import add,sub
+
+def test_add():
+    assert add(3,4)==7 # assert is the functions which check whether the condition is True or False 
+    assert add(9,4)==5
+
+
+def test_sub():
+    assert sub(8,3)==5
+    assert sub(45,4)==41
